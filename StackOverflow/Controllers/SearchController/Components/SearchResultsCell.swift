df29ca5bdc4c12f8fb746ad5c54a9dbafdfb739a
Gugulethu <mhlangagc@users.noreply.github.com>
2021-02-08 01:27:18 +0200

    Built the cell to show the search results

--- StackOverflow/Controllers/SearchController/Components/SearchResultsCell.swift
+++ StackOverflow/Controllers/SearchController/Components/SearchResultsCell.swift
@@ -10,6 +10,12 @@ import UIKit
 
 final class SearchResultsCell: BaseTableCell {
     
+    var question: Question? {
+        didSet {
+            guard let question = question else { return }
+        }
+    }
+    
     lazy var questionAnsweredImageView: UIImageView = {
         let imageView = UIImageView()
         imageView.contentMode = .scaleAspectFit
@@ -20,6 +26,7 @@ final class SearchResultsCell: BaseTableCell {
     
     lazy var titleLabel: UILabel = {
         let label = UILabel()
+        label.text = "How to make a UITextField move up when keyboard is present?"
         label.font = Font.regular14
         label.textColor = Colour.primary
         label.numberOfLines = 0
@@ -29,14 +36,26 @@ final class SearchResultsCell: BaseTableCell {
     
     lazy var authorLabel: UILabel = {
         let label = UILabel()
+        label.text = "asked by Nathan"
         label.font = Font.regular10
         label.textColor = Colour.grey
         label.translatesAutoresizingMaskIntoConstraints = false
         return label
     }()
     
+    lazy var titleStackView: UIStackView = {
+        let stackView = UIStackView(arrangedSubviews: [titleLabel, authorLabel])
+        stackView.axis = .vertical
+        stackView.distribution = .fillProportionally
+        stackView.alignment = .leading
+        stackView.spacing = Layout.spacing8
+        stackView.translatesAutoresizingMaskIntoConstraints = false
+        return stackView
+    }()
+    
     lazy var votesLabel: UILabel = {
         let label = UILabel()
+        label.text = "0 Votes"
         label.font = Font.regular10
         label.textColor = Colour.grey
         label.translatesAutoresizingMaskIntoConstraints = false
@@ -45,6 +64,7 @@ final class SearchResultsCell: BaseTableCell {
     
     lazy var answerLabel: UILabel = {
         let label = UILabel()
+        label.text = "2 Answers"
         label.font = Font.regular10
         label.textColor = Colour.grey
         label.translatesAutoresizingMaskIntoConstraints = false
@@ -53,27 +73,39 @@ final class SearchResultsCell: BaseTableCell {
     
     lazy var viewsLabel: UILabel = {
         let label = UILabel()
+        label.text = "10 Views"
         label.font = Font.regular10
         label.textColor = Colour.grey
         label.translatesAutoresizingMaskIntoConstraints = false
         return label
     }()
     
-    lazy var disclosureIndicatorImageView: UIImageView = {
-        let imageView = UIImageView()
-        imageView.contentMode = .scaleAspectFit
-        imageView.clipsToBounds = true
-        imageView.translatesAutoresizingMaskIntoConstraints = false
-        return imageView
+    lazy var voteAnswersViewStackView: UIStackView = {
+        let stackView = UIStackView(arrangedSubviews: [votesLabel, answerLabel, viewsLabel])
+        stackView.axis = .vertical
+        stackView.distribution = .fillEqually
+        stackView.alignment = .leading
+        stackView.spacing = Layout.spacing8
+        stackView.translatesAutoresizingMaskIntoConstraints = false
+        return stackView
     }()
-    
-    var question: Question? {
-        didSet {
-            guard let question = question else { return }
-        }
-    }
     
     override func configureView() {
         super.configureView()
+        accessoryType = .disclosureIndicator
+        layoutView()
+    }
+    
+    private func layoutView() {
+        addSubview(voteAnswersViewStackView)
+        addSubview(titleStackView)
+        
+        voteAnswersViewStackView.rightAnchor.constraint(equalTo: rightAnchor, constant: Layout.spacing20).isActive = true
+        voteAnswersViewStackView.centerYAnchor ->> centerYAnchor
+        
+        titleStackView.leftAnchor.constraint(equalTo: leftAnchor, constant: Layout.spacing18).isActive = true
+        titleStackView.topAnchor.constraint(equalTo: topAnchor, constant: Layout.spacing10).isActive = true
+        titleStackView.bottomAnchor.constraint(equalTo: bottomAnchor, constant: -Layout.spacing10).isActive = true
+        titleStackView.rightAnchor ->> voteAnswersViewStackView.leftAnchor
     }
 }
