c7ceebdb51ed5e59405952e5cf7048de2413515c
Gugulethu <mhlangagc@users.noreply.github.com>
2021-02-08 00:02:48 +0200

    Updated the Font Inteface

--- StackOverflow/Controllers/SearchController/Components/SearchResultsCell.swift
+++ StackOverflow/Controllers/SearchController/Components/SearchResultsCell.swift
@@ -10,6 +10,32 @@ import UIKit
 
 final class SearchResultsCell: BaseTableCell {
     
+    lazy var titleLabel: UILabel = {
+        let label = UILabel()
+        label.font = UIFont.systemFont(ofSize: 14.0, weight: .regular)
+        label.textColor = Colour.primary
+        label.numberOfLines = 0
+        label.translatesAutoresizingMaskIntoConstraints = false
+        return label
+    }()
+    
+    lazy var authorLabel: UILabel = {
+        let label = UILabel()
+        label.font = UIFont.systemFont(ofSize: 10.0, weight: .regular)
+        label.textColor = Colour.grey
+        label.translatesAutoresizingMaskIntoConstraints = false
+        return label
+    }()
+    
+    lazy var votesLabel: UILabel = {
+        let label = UILabel()
+        label.font = UIFont.systemFont(ofSize: 10.0, weight: .regular)
+        label.textColor = Colour.grey
+        label.numberOfLines = 0
+        label.translatesAutoresizingMaskIntoConstraints = false
+        return label
+    }()
+    
     var question: Question? {
         didSet {
             guard let question = question else { return }
