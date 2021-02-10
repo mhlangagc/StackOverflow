89e86bde455d89700f125a76f4b0f0c3bbdcebe6
Gugulethu <mhlangagc@users.noreply.github.com>
2021-02-10 03:34:05 +0200

    URL Center Tests Added

---
+++ StackOverflowTests/NetworkTests/NetworkControllerTests.swift
@@ -0,0 +1,46 @@
+//
+//  NetworkControllerTests.swift
+//  StackOverflowTests
+//
+//  Created by Gugulethu on 2021/02/10.
+//
+
+import Foundation
+import XCTest
+@testable import StackOverflow
+
+class NetworkControlTests: XCTestCase {
+    
+    var implementationUnderTest: SearchNetworkController!
+    
+    override func setUp() {
+        super.setUp()
+        implementationUnderTest = resolve(SearchNetworkProtocol.self)
+    }
+    
+    override func tearDown() {
+        super.tearDown()
+        implementationUnderTest = nil
+    }
+    
+    /*
+     Search For Results
+     */
+    func testSearchForQueryReturnsSuccessfully() {
+        let mockQuery = "swift"
+        let waitingForCompletionExpectation = expectation(description: "Waiting for success block to be called")
+        let alwaysBlockExpectation = expectation(description: "Waiting for always block to be called")
+        
+        implementationUnderTest.searchForAnswers(withQuery: mockQuery, always: {
+            alwaysBlockExpectation.fulfill()
+        }, onSuccess: { (searchResults) in
+            waitingForCompletionExpectation.fulfill()
+            XCTAssertNotNil(searchResults)
+        }) { (error) in
+            XCTAssertNil(error)
+            XCTFail("Error testing the search for query function")
+        }
+        wait(for: [waitingForCompletionExpectation, alwaysBlockExpectation], timeout: 5)
+    }
+
+}
